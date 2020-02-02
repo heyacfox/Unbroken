b1cce8356fa6758fee7bfd6a592a1eb8cb44e79f
A Code Fox <heyacfox@gmail.com>
2020-02-02 09:10:23 -0500

    New UI Added

--- Assets/03-Scripts/InputResultIndicator.cs
+++ Assets/03-Scripts/InputResultIndicator.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class InputResultIndicator : MonoBehaviour
 {
     public float upSpeed = 1f;
-    public float timeUntilDestroy = 2f;
+    public float timeUntilDestroy = 1f;
+    public float ySpawnOffset = 2f;
+    public float xSpawnOffset = -1f;
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, upSpeed);
+        this.transform.position = new Vector2(this.transform.position.x + xSpawnOffset, transform.position.y + ySpawnOffset);
         StartCoroutine(destroyAfterTime());
     }
 
     IEnumerator destroyAfterTime()
     {
         yield return new WaitForSeconds(timeUntilDestroy);
-        Destroy(this);
+        Destroy(this.gameObject);
     }
 
     
